061e4af5f9218565a288c9887d18ae0ee1e9d2c8
IhorBaiura <ihorbaiura@gmail.com>
2024-06-23 16:13:41 +0200

    feat: add a program to count base text statistic (blanks, tabs, newlines)

---
+++ 011_base_texts_statistic_blanks_tabs_newlines.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int b, t, nl, c;
+
+  b = t = nl = 0;
+  while ((c = getchar()) != EOF) {
+    if (c == '\n')
+      ++nl;
+
+    if (c == '\t')
+      ++t;
+
+    if (c == ' ')
+      ++b;
+  }
+
+  printf("Base text statistic:\nBlankes: %d\nTabs: %d\nNewlines: %d\n", b, t,
+         nl);
+}
