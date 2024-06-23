7ad9d20e584549b4586af388d3813ae0391c3084
IhorBaiura <ihorbaiura@gmail.com>
2024-06-23 17:48:22 +0200

    feat: add a task description for each solution

--- 013_visualise_tabs_backspaces_and_backslashes_in_the_text.c
+++ 013_visualise_tabs_backspaces_and_backslashes_in_the_text.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
-
 /*
- *
-  Exercise 1-10. Write a program to copy its input to its output, replacing each
-  tab by \t, each backspace by \b, and each backslash by \\. This makes tab and
-  backspaces visible in an unambiguous way.
- *
+ * Exercise 1-10. Write a program to copy its input to its output, replacing
+ * each tab by \t, each backspace by \b, and each backslash by \\. This makes
+ * tab and backspaces visible in an unambiguous way.
  */
+
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
   int c, esc;
