f783330a88d9d1a3914397ea979c892f00f55d28
IhorBaiura <ihorbaiura@gmail.com>
2024-06-25 12:44:22 +0200

    feat: add revised print longest line program

---
+++ 022_revised_print_longest_line.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int get_line(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* print longest input line */
+int main(int argc, char *argv[]) {
+  int len;
+  int max;
+  int c;
+  char line[MAXLINE];
+  char longest[MAXLINE];
+
+  max = 0;
+  while ((len = get_line(line, MAXLINE)) > 0) {
+    if (line[len - 1] != EOF || line[len - 1] != '\n') {
+      while ((c = getchar())) {
+        if (c == EOF || c == '\n')
+          break;
+        len++;
+      }
+    }
+
+    if (len > max) {
+      max = len;
+      copy(longest, line);
+    }
+  }
+  if (max > 0)
+    printf("\nSize of longest line: %d\nLongest line:\n%s", max, longest);
+  return 0;
+}
+
+/* get_line: read a line into s, return length */
+int get_line(char s[], int lim) {
+  int c, i;
+
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+    s[i] = c;
+  if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';
+  return i;
+}
+
+/* copy: copy 'from' into 'to'; assune to is big enough */
+void copy(char to[], char from[]) {
+  int i;
+
+  i = 0;
+  while ((to[i] = from[i]) != '\0')
+    ++i;
+}
