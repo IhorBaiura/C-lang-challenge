412a0afddb5cf5719c492e852a426b5110571d8e
IhorBaiura <ihorbaiura@gmail.com>
2024-06-25 11:19:09 +0200

    feat: add program to print lingest line & update power program

--- 020_power.c
+++ 020_power.c
@@ -13,10 +13,9 @@ int main(int argc, char *argv[]) {
 
 /* power: raise base to n-th power; n >= 0 */
 int power(int base, int n) {
-  int i, p;
+  int p;
 
-  p = 1;
-  for (i = 1; i <= n; ++i)
+  for (p = 1; n > 0; n--)
     p = p * base;
   return p;
 }
