6cd1e5e547e6ddecd9b9e987ebcb3bbe925f124a
IhorBaiura <ihorbaiura@gmail.com>
2024-07-17 09:41:39 +0200

    feat: add formatted output of data type value representation

--- 032_type_sizes.c
+++ 032_type_sizes.c
@@ -5,6 +5,7 @@
  * determine the ranges of the various floating-point types.
  */
 
+#include <float.h>
 #include <limits.h>
 #include <stdio.h>
 
@@ -61,6 +62,14 @@ int main(int argc, char *argv[]) {
          sizeof(sl), sizeof(sl) * CHAR_BIT, (long)LONG_MIN, (long)LONG_MAX);
   printf("| unsigned long\t\t|\t%lu\t\t|\t%lu\t\t|\t%d\t\t| %lu\t|\n",
          sizeof(ul), sizeof(ul) * CHAR_BIT, 0, (unsigned long)ULONG_MAX);
+
+  printf("\n\n float types");
+  printf("\n| float\t\t\t|\t%lu\t\t|\t%lu\t\t|\t%g\t|\t%g\t|\n", sizeof(float),
+         sizeof(float) * CHAR_BIT, (float)FLT_MAX, (float)FLT_MIN);
+  printf("| -float\t\t|\t%lu\t\t|\t%lu\t\t|\t%g\t|\t%g\t|\n", sizeof(float),
+         sizeof(float) * CHAR_BIT, (float)-FLT_MAX, (float)-FLT_MIN);
+  printf("| double\t\t|\t%lu\t\t|\t%lu\t\t|\t%g\t|\t%g\t|\n", sizeof(double),
+         sizeof(double) * CHAR_BIT, (double)DBL_MAX, (double)DBL_MIN);
 
   return 0;
 }
