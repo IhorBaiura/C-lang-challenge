3e01199f0f60be53b267cdcdef5821abb3a46f23
IhorBaiura <ihorbaiura@gmail.com>
2024-07-20 00:46:20 +0200

    feat: add some experements with escape sequences

--- 033_escapes.c
+++ 033_escapes.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("\a");
+  // printf("\a");
   printf("aaaa aaaa aaaa\rbbbb cccc dd\n");
   printf("first\vstring\vagain\n");
   printf("first\tsecond\tthird\n");
@@ -9,6 +9,8 @@ int main(int argc, char *argv[]) {
   printf("first\b\b\b second\n");
 
   printf("first\fstring\fagain\n");
+
+  printf("%d\n", '\0');
 
   return 0;
 }
