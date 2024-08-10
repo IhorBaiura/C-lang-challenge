5fc5b52e8b44244d6c21ed14162caf82ffba0c0a
IhorBaiura <ihorbaiura@gmail.com>
2024-08-10 18:09:35 +0200

    feat: add extension of scientific notation to atof function

--- 048_atof/atof.c
+++ 048_atof/atof.c
@@ -8,11 +8,13 @@
  */
 
 #include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
 
 /* atof: convert string s to double */
-double atof(char s[]) {
+double _atof(char s[]) {
   double val, power;
-  int i, sign;
+  int i, sign, k, exp;
   for (i = 0; isspace(s[i]); i++) /* skip white space */
     ;
   sign = (s[i] == '-') ? -1 : 1;
@@ -25,6 +27,26 @@ double atof(char s[]) {
   for (power = 1.0; isdigit(s[i]); i++) {
     val = 10.0 * val + (s[i] - '0');
     power *= 10.0;
+  }
+  if (s[i] == 'e' || s[i] == 'E') {
+    ++i;
+    if (s[i] == '-') {
+      while (isdigit(s[i])) {
+        s[k++] = s[i];
+      }
+      if (k > 0)
+        s[k] = '\0';
+      exp = pow(10, atoi(s));
+      power /= exp;
+    } else if (s[i] == '+') {
+      while (isdigit(s[i])) {
+        s[k++] = s[i];
+      }
+      if (k > 0)
+        s[k] = '\0';
+      exp = pow(10, atoi(s));
+      power *= exp;
+    }
   }
   return sign * val / power;
 }
