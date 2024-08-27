323c8134137d26d7eadb5a61a60e7f710c4ae955
IhorBaiura <ihorbaiura@gmail.com>
2024-08-27 08:40:31 +0200

    feat: add refactoring to get_line function

--- 023_print_longest_lines.c
+++ 023_print_longest_lines.c
@@ -22,15 +22,17 @@ int main(int argc, char *argv[]) {
 }
 
 /* get_line: read a line into s, return length */
-int get_line(char s[], int lim) {
-  int c, i;
+int get_line(char *s, int lim) {
+  int c;
+  char *t;
 
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
-    s[i] = c;
+  for (--lim; lim > 0 && (c = getchar()) != EOF && c != '\n'; lim--)
+    *s++ = c;
+
   if (c == '\n') {
-    s[i] = c;
-    ++i;
+    *s++ = c;
   }
-  s[i] = '\0';
-  return i;
+
+  *s = '\0';
+  return s - t;
 }
