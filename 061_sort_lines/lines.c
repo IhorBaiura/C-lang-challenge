a12869508d51323c07a98b11176c04277d77b61a
IhorBaiura <ihorbaiura@gmail.com>
2024-08-30 21:05:03 +0200

    feat: add line buffer to lines sort program

--- 061_sort_lines/lines.c
+++ 061_sort_lines/lines.c
@@ -1,23 +1,36 @@
+/*
+ *
+ * Exercise 5-7. Rewrite readlines to store lines in an array supplied by main,
+ * rather than calling alloc to maintain storage. How much faster is the
+ * program?
+ *
+ */
+
 #include <stdio.h>
 #include <string.h>
 
-#define MAXLEN 1000 /* max length of any input line */
+#define MAXLEN 1000     /* max length of any input line */
+#define BUFFERSIZE 7000 /* max space for keeping lines */
 
 int getline_m(char *, int);
 char *alloc(int);
 
 /* readlines: read input lines */
-int readlines(char *lineptr[], int maxlines) {
+int readlines(char *lineptr[], char *linebuffer, int maxlines) {
   int len, nlines;
-  char *p, line[MAXLEN];
+  char *p = linebuffer;
+  char *linestop = linebuffer + BUFFERSIZE;
+  char line[MAXLEN];
+
   nlines = 0;
   while ((len = getline_m(line, MAXLEN)) > 0)
-    if (nlines >= maxlines || (p = alloc(len)) == NULL)
+    if (nlines >= maxlines || p + len > linestop)
       return -1;
     else {
       line[len - 1] = '\0'; /* delete newline */
       strcpy(p, line);
       lineptr[nlines++] = p;
+      p += len;
     }
   return nlines;
 }
--- 061_sort_lines/main.c
+++ 061_sort_lines/main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 #define MAXLINES 5000 /* max #lines to be sorted */
+#define BUFFERSIZE 7000
 
-char *lineptr[MAXLINES]; /* pointers to text lines */
-int readlines(char *lineptr[], int nlines);
+char *lineptr[MAXLINES];            /* pointers to text lines */
+char buffer[MAXLINES * BUFFERSIZE]; // Pre-allocated buffer for lines
+
+int readlines(char *lineptr[], char *linebuffer, int maxlines);
 void writelines(char *lineptr[], int nlines);
 void qsort(char *lineptr[], int left, int right);
 
 /* sort input lines */
 int main() {
   int nlines; /* number of input lines read */
-  if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+  if ((nlines = readlines(lineptr, buffer, MAXLINES)) >= 0) {
     qsort(lineptr, 0, nlines - 1);
     writelines(lineptr, nlines);
     return 0;
