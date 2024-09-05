30c3ec35c48894789601c1f37365ceba874d6abe
IhorBaiura <ihorbaiura@gmail.com>
2024-09-05 18:27:01 +0200

    feat(P-062): add implementation for dynamic stack

---
+++ 062_stack/stack_dynamic.c
@@ -0,0 +1,38 @@
+#include "stack_dynamic.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void print(Stack *st) {
+  printf("Stack: ");
+  for (unsigned int i = 0; i < st->n; i++)
+    printf("%d ", st->a[i]);
+  printf("\n");
+}
+
+void init(Stack *st) {
+  st->n = 0;
+  st->a = NULL;
+  st->size = 0;
+}
+
+void push(Stack *st, Data d) {
+  if (is_full(st)) {
+    st->size += STEP;
+    st->a = realloc(st->a, st->size * sizeof(st->a[0]));
+  }
+
+  st->a[st->n++] = d;
+}
+
+Data pop(Stack *st) {
+  if (is_empty(st)) {
+    printf("Stack is empty");
+    return 0;
+  }
+
+  return st->a[st->n-- - 1];
+}
+
+int is_empty(Stack *st) { return st->n == 0; }
+
+int is_full(Stack *st) { return st->n >= st->size; }
