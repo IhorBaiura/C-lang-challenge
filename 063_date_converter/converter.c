4ee96b9497f81f18564912c83283c942b360de2a
IhorBaiura <ihorbaiura@gmail.com>
2024-09-08 09:21:24 +0200

    feat(P-063): rewrite the routines with pointers instead of indexing

--- 063_date_converter/converter.c
+++ 063_date_converter/converter.c
@@ -3,6 +3,9 @@
  * Exercise 5-8. There is no error checking in day_of_year or month_day. Remedy
  * this defect.
  *
+ * Exercise 5-9. Rewrite the routines day_of_year and month_day with
+ * pointers instead of indexing.
+ *
  */
 
 #include "converter.h"
@@ -13,7 +16,8 @@ static char daytab[2][13] = {
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
 
 int day_of_year(int year, int month, int day) {
-  int i, leap;
+  int leap;
+  char *p;
 
   if (month < 1 || month > 12) {
     printf("Wrong month was recieved!\n");
@@ -21,20 +25,22 @@ int day_of_year(int year, int month, int day) {
   }
 
   leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+  p = daytab[leap];
 
   if (day < 1 || day > daytab[leap][month]) {
     printf("Wrong day was recieved!\n");
     return -1;
   }
 
-  for (i = 1; i < month; i++)
-    day += daytab[leap][i];
+  while (--month)
+    day += *++p;
 
   return day;
 }
 
 void month_day(int year, int yearday, int *pmonth, int *pday) {
-  int i, leap;
+  int leap;
+  char *p;
 
   if (year < 1) {
     *pmonth = -1;
@@ -43,17 +49,13 @@ void month_day(int year, int yearday, int *pmonth, int *pday) {
   }
 
   leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+  p = daytab[leap];
 
-  for (i = 1; yearday > daytab[leap][i]; i++)
-    yearday -= daytab[leap][i];
+  while (yearday > *++p)
+    yearday -= *p;
 
-  if (i > 12 && yearday > daytab[leap][i]) {
-    *pday = -1;
-    *pmonth = -1;
-  } else {
-    *pday = yearday;
-    *pmonth = i;
-  }
+  *pday = yearday;
+  *pmonth = p - *(daytab + leap);
 }
 
 char *month_name(int n) {
