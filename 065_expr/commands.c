f38799d8b06d6364985e7daf74ae82e0032e4cb8
IhorBaiura <ihorbaiura@gmail.com>
2024-09-13 21:59:05 +0200

    feat(IB-065): add updates to commands to expr calculator

--- 065_expr/commands.c
+++ 065_expr/commands.c
@@ -1,13 +1,21 @@
 #include "commands.h"
 #include <math.h>
 
+void apply_command(Stack *st, char *command) {}
+
 void add(Stack *st) { push(st, pop(st) + pop(st)); }
 
-void subtract(Stack *st) { push(st, pop(st) + pop(st)); }
+void subtract(Stack *st) {
+  Data tmp = pop(st);
+  push(st, pop(st) - tmp);
+}
 
 void multiply(Stack *st) { push(st, pop(st) * pop(st)); }
 
-void divide(Stack *st) { push(st, pop(st) / pop(st)); }
+void divide(Stack *st) {
+  Data tmp = pop(st);
+  push(st, pop(st) / tmp);
+}
 
 void sin_c(Stack *st) { push(st, sin(pop(st))); }
 
@@ -19,10 +27,10 @@ void ctg_c(Stack *st) { push(st, 1 / tan(pop(st))); }
 
 void pow_c(Stack *st) {
   Data tmp = pop(st);
-  push(st, pow(tmp, pop(st)));
+  push(st, pow(pop(st), tmp));
 }
 
 void mod_c(Stack *st) {
   Data tmp = pop(st);
-  push(st, tmp % pop(st));
+  push(st, pop(st) % tmp);
 }
